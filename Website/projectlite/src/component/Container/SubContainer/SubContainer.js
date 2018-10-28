f408e5210f4b1ed5e376c375f457a0415cc9b659
Anthony Vu <tk42111@gmail.com>
2018-10-28 01:26:00 -0700

    added prop-types package

--- Website/projectlite/src/component/Container/SubContainer/SubContainer.js
+++ Website/projectlite/src/component/Container/SubContainer/SubContainer.js
@@ -1,14 +1,15 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import './stylesSubContainer.css';
 
 const SubContainer = (props) => {
     return (
-        <div class="inner cover">
-            <h1 class="cover-heading">{props.h1Text}</h1>
-            <p class="lead">{props.pText}</p>
-            <p class="lead">
-                <a href="features.html" class="btn btn-lg btn-default">{props.btnText}</a>
+        <div className="inner cover">
+            <h1 className="cover-heading">{props.h1Text}</h1>
+            <p className="lead">{props.pText}</p>
+            <p className="lead">
+                <a href="features.html" className="btn btn-lg btn-default">{props.btnText}</a>
             </p>
         </div>
     );
@@ -18,7 +19,7 @@ SubContainer.propTypes = {
     h1Text: PropTypes.string,
     pText: PropTypes.string,
     btnText: PropTypes.string
-}
+};
 
 export default SubContainer;
 
