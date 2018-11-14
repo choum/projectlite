408fb530c0d9c6c2eeaac8615f9a8caa810e6418
Anthony Vu <tk42111@gmail.com>
2018-11-14 10:42:55 -0800

    done

--- Website/projectlite/src/components/Container/MainContainer/MainContainer.js
+++ Website/projectlite/src/components/Container/MainContainer/MainContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./stylesMainContainer.css";
 
 const MainContainer = ({ children }) => {
-  return <div classname="main">{children}</div>;
+  return <div className="main">{children}</div>;
 };
 
 export default MainContainer;
