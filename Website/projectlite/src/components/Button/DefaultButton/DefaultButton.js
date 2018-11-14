5099f1993f5746e5dab14904ff051907ec66f0f2
Anthony Vu <tk42111@gmail.com>
2018-11-14 14:34:38 -0800

    added file restructure

---
+++ Website/projectlite/src/components/Button/DefaultButton/DefaultButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+const DefaultButton = props => {
+  return <Button className="btn1">{props.text}</Button>;
+};
+
+export default DefaultButton;
