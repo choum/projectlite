6fc05c02a98c5f76bf8dff4ab3afcfdd3d883c42
Heather <dylanchoum123@yahoo.com>
2018-12-20 21:45:47 -0800

    header and footer to styled components

--- Website/projectlite/src/components/Footer/Footer.js
+++ Website/projectlite/src/components/Footer/Footer.js
@@ -1,16 +1,27 @@
 import React from "react";
 
 import { DefaultContainer } from "../Container";
+import styled from "styled-components";
 
-import "./stylesFooter.css";
+const Foot = styled.footer`
+position: relative;
+margin-top: -60px;
+/*width: 100%;*/
+height: 60px;
+clear: both;
+
+background-color: #282828;
+padding: 20px;
+color: #fff;
+`
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <Foot className="footer">
       <DefaultContainer>
         <span>This is a footer.</span>
       </DefaultContainer>
-    </footer>
+    </Foot>
   );
 };
 
