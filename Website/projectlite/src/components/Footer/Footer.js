cffe7788e4ab1b43561cd12283ee3a7179d5ffcc
Heather <dylanchoum123@yahoo.com>
2019-01-04 23:55:09 -0800

    fixed footer not going to bottom of theentire page

--- Website/projectlite/src/components/Footer/Footer.js
+++ Website/projectlite/src/components/Footer/Footer.js
@@ -4,13 +4,9 @@ import { DefaultContainer } from "../Container";
 import styled from "styled-components";
 
 const Foot = styled.footer`
-position: fixed;
-margin-top: -60px;
-/*width: 100%;*/
-height: 60px;
-clear: both;
-bottom: 0px;
-width: 100%;
+position: absolute;
+bottom: 0;
+width:100%;
 background-color: #282828;
 padding: 20px;
 color: #fff;
