bc911a7c74e35afc84ad8b5f1cfc5286cf2f9856
Anthony Vu <tk42111@gmail.com>
2019-01-06 21:18:36 -0800

    converted LightBox to React Comp

--- Website/projectlite/src/components/LightsBox.js
+++ Website/projectlite/src/components/LightsBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import CardContainer from "./Container/CardContainer";
 import styled from "styled-components";
 import {
@@ -15,22 +15,34 @@ const Border = styled.div`
   background-color: yellow;
 `;
 
-const LightsBox = ({title, clusterData}) => (
-  <CardContainer type="bodyheader" title={title}>
-    <Border>
-      {console.log(clusterData)}
-      <HexGrid width={"100%"} height={"100%"}>
-        <Layout
-          size={{ x: 10, y: 10 }}
-          flat={true}
-          spacing={1.1}
-          origin={{ x: 0, y: 0 }}
-        >
-          <Hexagon q={0} r={0} s={0} />
-        </Layout>
-      </HexGrid>
-    </Border>
-  </CardContainer>
-);
+class LightsBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+
+  render() {
+    const { title, clusterData } = this.props;
+
+    return (
+      <CardContainer type="bodyheader" title={title}>
+        <Border>
+          {console.log(clusterData)}
+          <HexGrid width={"100%"} height={"100%"}>
+            <Layout
+              size={{ x: 10, y: 10 }}
+              flat={true}
+              spacing={1.1}
+              origin={{ x: 0, y: 0 }}
+            >
+              <Hexagon q={0} r={0} s={0} />
+            </Layout>
+          </HexGrid>
+        </Border>
+      </CardContainer>
+    );
+  }
+}
 
 export default LightsBox;
