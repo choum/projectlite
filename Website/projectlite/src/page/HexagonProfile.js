f250d3994212ac49228aa15cf5bf02712fec3cdc
Heather <dylanchoum123@yahoo.com>
2019-01-09 19:41:44 -0800

    finished sidenav 1 and toggle functionality added

--- Website/projectlite/src/page/HexagonProfile.js
+++ Website/projectlite/src/page/HexagonProfile.js
@@ -12,7 +12,7 @@ import "react-toggle-component/styles.css"
 const Navigation = styled.div`
   background-color: #282828;
   color: white;
-  height: 93vh;
+  height: 100vh;
   width: 100%;
 `
 const MenuType = styled.h4`
@@ -26,106 +26,78 @@ const ItemType = styled.h5`
 const Divider = styled.hr`
   border-color: white;
 `
+const Wrapper = styled.span`
+  border-top: 3px solid white;
+  border-bottom: 3px solid white;
+`
 class HexagonProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      checked: ''
     };
   }
 
-  category() {
-    if (this.state.checked) {
 
+  renderTitle() {
+    if (this.state.checked === true) {
+      return (<MenuType>Advanced Customization</MenuType>);
     } else {
-
+      return (<MenuType>Simple Customization</MenuType>);
     }
   }
 
-  renderSimple() {
-    return (
-    <div className="row">
-        <div className="col-md-2">
-        <Navigation>
-          <SideNav defaultSelectedPath="1">
-            <SlimContainer>
-              <MenuType>Simple Customization</MenuType>
-              <Divider/>
-              <div className="effect-block">
-              <ItemType>Effect:</ItemType>
-              <select className="form-control form-control-sm">
-                <option>Static Color</option>
-                <option>Wave</option>
-              </select>
-              </div>
-              <Divider/>
-              <div className="orientation-block">
-                <ItemType>Orientation:</ItemType>
-                <label>
-                  <input type="radio" name="options" id="option1" autocomplete="off" checked />Flat
-                </label>
-                <br/>
-                <label>
-                  <input type="radio" name="options" id="option2" autocomplete="off" checked />Pointy
-                </label>
-              </div>
-              <Divider/>
-            </SlimContainer>
-          </SideNav>
-        </Navigation>
-        </div>
-        {this.renderCluster()}
-        <div className="col-md-2">
-          <Navigation>
-            <SideNav>
-            </SideNav>
-          </Navigation>
-        </div>
-      </div>
-    );
-  }
-
-  renderAdvanced() {
-    return (
-      <div className="row">
-          <div className="col-md-2">
-          <Navigation>
-            <SideNav defaultSelectedPath="1">
-              <SlimContainer>
-                <MenuType>Advanced Customization</MenuType>
-                <Divider/>
-                <div className="effect-block">
-                <ItemType>Effect:</ItemType>
-                <select className="form-control form-control-sm">
-                  <option>Static Color</option>
-                  <option>Wave</option>
-                </select>
-                </div>
-                <Divider/>
-                <div className="orientation-block">
-                  <ItemType>Orientation:</ItemType>
-                  <label>
-                    <input type="radio" name="options" id="option1" autocomplete="off" checked />Flat
-                  </label>
-                  <br/>
-                  <label>
-                    <input type="radio" name="options" id="option2" autocomplete="off" checked />Pointy
-                  </label>
-                </div>
-                <Divider/>
-              </SlimContainer>
-            </SideNav>
-          </Navigation>
-          </div>
-          {this.renderCluster()}
-          <div className="col-md-2">
+  renderSideNav() {
+      return (
+        <div className="row">
+            <div className="col-md-2">
             <Navigation>
-              <SideNav>
+              <SideNav defaultSelectedPath="1">
+                <SlimContainer>
+                  {this.renderTitle()}
+                  <Divider/>
+                  <div className="effect-block">
+                  <ItemType>Effect:</ItemType>
+                  <select value={this.state.selectValue} onChange={e=>{
+                    this.setState({
+                      selectValue: e.target.value
+                    })
+                  }} className="form-control">
+                    <option>Static Color</option>
+                    <option>Wave</option>
+                  </select>
+                  </div>
+                  <Divider/>
+                  <div className="orientation-block">
+                    <ItemType>Orientation:</ItemType>
+                    <label>
+                      <input type="radio" name="options" id="option1" value="Flat"/>Flat
+                    </label>
+                    <br/>
+                    <label>
+                      <input type="radio" name="options" id="option2" value="Pointy"/>Pointy
+                    </label>
+                  </div>
+                  <Divider/>
+                  {this.state.selectValue === "Wave" && this.state.checked === true &&
+                  <div className="pick-block">
+                      <ItemType>Pick Order</ItemType>
+                      <button className="btn btn-light" value="Pick">Pick</button>
+                    </div>
+                  }
+                </SlimContainer>
               </SideNav>
             </Navigation>
+            </div>
+            {this.renderCluster()}
+            <div className="col-md-2">
+              <Navigation>
+                <SideNav>
+                </SideNav>
+              </Navigation>
+            </div>
           </div>
-        </div>
-    );
+      );
   }
 
   renderCluster() {
@@ -142,7 +114,7 @@ class HexagonProfile extends Component {
     render() {
       return (
       <MainContainer>
-        {this.state.checked === true ? this.renderAdvanced() : this.renderSimple()}
+        {this.renderSideNav()}
       </MainContainer>
     );
     }
