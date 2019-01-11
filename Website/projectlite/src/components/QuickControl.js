2af09ab8baaeb2e67344cab48839eba8ce563b83
Heather <hhtran@cpp.edu>
2019-01-11 11:42:34 -0800

    changed lightbulb color

--- Website/projectlite/src/components/QuickControl.js
+++ Website/projectlite/src/components/QuickControl.js
@@ -18,7 +18,7 @@ const Box = styled.div`
 `;
 
 const offColor = "#CDCDCD";
-const onColor = "#41FE58";
+const onColor = "#F3DE8A";
 
 const QuickControl = ({ value, onChange, title }) => (
   <CardContainer type="bodyheader" title={title}>
