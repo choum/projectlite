260bdeec42510266392b0a70d9127013eeaf5a28
Heather <dylanchoum123@yahoo.com>
2019-01-17 23:12:05 -0800

    settings page

---
+++ Website/projectlite/src/components/Form/Billing.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import styled from "styled-components";
+
+class Billing extends Component {
+  render() {
+    return (
+      <form>
+        <div class="form-group">
+          <label for="inputAddress">Address</label>
+          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+        </div>
+        <div class="form-group">
+          <label for="inputAddress2">Address 2</label>
+          <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputCity">City</label>
+            <input type="text" class="form-control" id="inputCity"/>
+          </div>
+          <div class="form-group col-md-4">
+            <label for="inputState">State</label>
+            <select id="inputState" class="form-control">
+              <option selected>Choose...</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div class="form-group col-md-2">
+            <label for="inputZip">Zip</label>
+            <input type="text" class="form-control" id="inputZip"/>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+export default Billing;
