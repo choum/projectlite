31bb9bad0f812b71a9f5c3882f68a9429d5d9ab4
Anthony Vu <tk42111@gmail.com>
2019-02-15 23:11:52 -0800

    start: forgot password page

--- Website/projectlite/src/constants/routes.js
+++ Website/projectlite/src/constants/routes.js
@@ -6,5 +6,6 @@ export const LOGIN = "/login";
 export const SIGNOUT = "/sign-out";
 export const SIGNUP = "/sign-up";
 export const HEXAGONPROFILE = "/hexagon-profile/:id";
+export const FORGOTPASSWORD = "/forgot-password";
 
 export const EMULATOR = "/emu";
