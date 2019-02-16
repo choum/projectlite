1cd1e3544dcb388cbd1a8fa97ce6022740f04b0e
Anthony Vu <tk42111@gmail.com>
2019-02-15 18:28:06 -0800

    create: moved code from index to App.js and abstracted header.js to mobile and desktop

--- Website/projectlite/src/index.js
+++ Website/projectlite/src/index.js
@@ -2,42 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Firebase, { FirebaseContext } from "./components/Firebase";
-
-import Landing from "./page/Landing";
-import About from "./page/About";
-import Dashboard from "./page/Dashboard";
-import Login from "./page/Login";
-import Signout from "./page/Signout";
-import HexagonProfile from "./page/HexagonProfile";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { WrapContainer } from "./components/Container";
-import Emulator from "./page/Emulator";
-import SignUp from "./page/SignUp";
-
-import * as ROUTES from "./constants/routes";
+import App from "./App";
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <Router>
-      <WrapContainer>
-        <Header login={false} />
-        <Switch>
-          <Route exact path={ROUTES.LANDING} component={Landing} />
-          <Route path={ROUTES.ABOUT} component={About} />
-          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGNOUT} component={Signout} />
-          <Route path={ROUTES.SIGNUP} component={SignUp} />
-          <Route path={ROUTES.EMULATOR} component={Emulator} />
-          <Route path={ROUTES.HEXAGONPROFILE} component={HexagonProfile} />
-        </Switch>
-        <Footer />
-      </WrapContainer>
-    </Router>
+    <App />
   </FirebaseContext.Provider>,
   document.getElementById("root")
 );
