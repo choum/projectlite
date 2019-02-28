abec5159fd3f418b857acb20b5eaddf1526e26fe
Ryan White <ryanswhite98@gmail.com>
2019-02-28 14:02:33 -0800

    Re-Add file to change name

---
+++ Website/projectlite/src/page/SignOut.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+import { MainContainer } from "../components/Container";
+
+import { withFirebase } from "../components/Firebase";
+
+class SignOut extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirectReady: false
+    };
+    this.firebase = this.props.firebase;
+    this.doSO = this.doSO.bind(this);
+    setTimeout(this.doSO, 100);
+  }
+
+  doSO = () => {
+    this.firebase.doSignOut();
+    this.setState({ redirectReady: true });
+  };
+
+  render() {
+    if (this.state.redirectReady === true) return <Redirect to="/" />;
+
+    return (
+      <MainContainer>
+        Signing out... <br />
+        Redirecting..
+      </MainContainer>
+    );
+  }
+}
+
+export default withFirebase(SignOut);
